c98f11ed528c90ac0ce66ce63920667604755153
viliger <wowlocale@gmail.com>
2024-12-05 19:11:36 +0300

    Added emission flicker for spider drone. Added emission going to zero on normal spider death.

--- EnemiesReturns/ModdedEntityStates/MechanicalSpider/Death/DeathNormal.cs
+++ EnemiesReturns/ModdedEntityStates/MechanicalSpider/Death/DeathNormal.cs
@@ -13,11 +13,37 @@ namespace EnemiesReturns.ModdedEntityStates.MechanicalSpider.Death
 
         private bool effectSpawned = false;
 
+        private Renderer modelRenderer;
+
+        private MaterialPropertyBlock propertyBlock;
+
+        private float initialEmission;
+
         public override void OnEnter()
         {
             bodyPreservationDuration = 1f;
             base.OnEnter();
             Util.PlaySound("ER_Spider_Death_Normal_Play", base.gameObject);
+
+            var childLocator = GetModelChildLocator();
+            modelRenderer = childLocator.FindChildComponent<Renderer>("Model");
+            if (modelRenderer)
+            {
+                propertyBlock = new MaterialPropertyBlock();
+                initialEmission = modelRenderer.material.GetFloat("_EmPower");
+                propertyBlock.SetFloat("_EmPower", initialEmission);
+                modelRenderer.SetPropertyBlock(propertyBlock);
+            }
+        }
+
+        public override void Update()
+        {
+            base.Update();
+            if (modelRenderer)
+            {
+                propertyBlock.SetFloat("_EmPower", Mathf.Lerp(initialEmission, 0, age / (bodyPreservationDuration - 0.1f)));
+                modelRenderer.SetPropertyBlock(propertyBlock);
+            }
         }
 
         public override void FixedUpdate()
