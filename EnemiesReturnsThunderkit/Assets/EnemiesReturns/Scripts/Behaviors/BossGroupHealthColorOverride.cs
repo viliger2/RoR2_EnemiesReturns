bc826b033e69b03d8119dbb561ee06871324e27c
viliger <wowlocale@gmail.com>
2025-03-07 14:35:42 +0300

    Implemented BossGroupHealthColorOverride

--- EnemiesReturnsThunderkit/Assets/EnemiesReturns/Scripts/Behaviors/BossGroupHealthColorOverride.cs
+++ EnemiesReturnsThunderkit/Assets/EnemiesReturns/Scripts/Behaviors/BossGroupHealthColorOverride.cs
@@ -1,31 +1,57 @@
 ﻿using RoR2;
 using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using System.Text;
 using UnityEngine;
 
 namespace EnemiesReturns.Behaviors
 {
+    // TODO: add scene component that just does on hooks for that specific scene
     [RequireComponent(typeof(BossGroup))]
     public class BossGroupHealthColorOverride : MonoBehaviour
     {
         public Color healthBarColorOverride;
 
+        public BossGroup bossGroup;
+
+        public readonly static Color defaultHealthBarColor = new Color(206f/255f, 3f/255f, 3f/255f);
+
+        public static ConditionalWeakTable<BossGroup, BossGroupHealthColorOverride> overrideDictionary = new ConditionalWeakTable<BossGroup, BossGroupHealthColorOverride>();
+
+        private void OnEnable()
+        {
+            if (!bossGroup)
+            {
+                bossGroup = GetComponent<BossGroup>();
+            }
+            overrideDictionary.Add(bossGroup, this);
+        }
+
+        private void OnDisable()
+        {
+            if (bossGroup)
+            {
+                overrideDictionary.Remove(this.gameObject.GetComponent<BossGroup>());
+            }
+        }
 
         // public static void Hooks()
         // {
-        //     On.RoR2.UI.HUDBossHealthBarController.OnClientDamageNotified += HUDBossHealthBarController_OnClientDamageNotified;
+        //     On.RoR2.UI.HUDBossHealthBarController.LateUpdate += HUDBossHealthBarController_LateUpdate;
         // }
 
-        // private static void HUDBossHealthBarController_OnClientDamageNotified(On.RoR2.UI.HUDBossHealthBarController.orig_OnClientDamageNotified orig, RoR2.UI.HUDBossHealthBarController self, DamageDealtMessage damageDealtMessage)
+        // private static void HUDBossHealthBarController_LateUpdate(On.RoR2.UI.HUDBossHealthBarController.orig_LateUpdate orig, RoR2.UI.HUDBossHealthBarController self)
         // {
-        //     orig(self, damageDealtMessage);
-        //     if (self.currentBossGroup && self.fillRectImage)
+        //     orig(self);
+        //     if(self.currentBossGroup && self.fillRectImage)
         //     {
-        //         var component = self.currentBossGroup.gameObject.GetComponent<BossGroupHealthColorOverride>();
-        //         if (component)
+        //         if(overrideDictionary.TryGetValue(self.currentBossGroup, out var component))
         //         {
         //             self.fillRectImage.color = component.healthBarColorOverride;
+        //         } else
+        //         {
+        //             self.fillRectImage.color = defaultHealthBarColor;
         //         }
         //     }
         // }
