63c0a33e64b0d2759d093eec7df6e39181972bf4
FORCED-REASSEMBLY <f9079520@gmail.com>
2025-04-11 22:06:52 +0100

    Archer bugs are now in game

---
+++ EnemiesReturns/Enemies/ArcherBug/ArcherBugBody.cs
@@ -0,0 +1,198 @@
+﻿using EnemiesReturns.Components;
+using EnemiesReturns.Components.BodyComponents;
+using EnemiesReturns.Components.BodyComponents.Skills;
+using EnemiesReturns.Components.GeneralComponents;
+using EnemiesReturns.Components.ModelComponents;
+using EnemiesReturns.PrefabSetupComponents.BodyComponents;
+using HG;
+using RoR2;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+namespace EnemiesReturns.Enemies.ArcherBug
+{
+    public class ArcherBugBody : BodyBase
+    {
+        public struct Skills
+        {
+            
+        }
+
+        public struct SkillFamilies
+        {
+            
+        }
+
+        public struct SkinDefs
+        {
+            public static SkinDef Default;
+        }
+
+        public struct SpawnCards
+        {
+      
+        }
+
+        public static GameObject BodyPrefab;
+
+        protected override bool AddHitBoxes => true;
+
+        
+
+        protected override IAimAssist.AimAssistTargetParams AimAssistTargetParams()
+        {
+            return new IAimAssist.AimAssistTargetParams()
+            {
+                assistScale = 2f,
+                pathToPoint0 = "ModelBase/Bug/Armature/root/Base/Body/head",
+                pathToPoint1 = "ModelBase/Bug/Armature/root"
+            };
+        }
+
+        protected override ICharacterBody.CharacterBodyParams CharacterBodyParams(Transform aimOrigin, Sprite icon)
+        {
+            return new ICharacterBody.CharacterBodyParams("ENEMIES_RETURNS_ARCHERBUG_BODY_NAME", GetCrosshair(), aimOrigin, icon, GetInitialBodyState())
+            {
+                mainRootSpeed = 33,
+                baseMaxHealth = 140,
+                levelMaxHealth = 42,
+                baseDamage = 15,
+                levelDamage = 3,
+                baseArmor = 0,
+                levelArmor = 0,
+                baseMoveSpeed = 8,
+                baseJumpCount = 1,
+                isChampion = false,
+                autoCalculateStats = true,
+
+            };
+        }
+
+        protected override ICharacterModel.CharacterModelParams CharacterModelParams(GameObject modelPrefab)
+        {
+            var bugBodyRenderer = modelPrefab.transform.Find("Bug").gameObject.GetComponent<SkinnedMeshRenderer>();
+            var bugWingsRenderer = modelPrefab.transform.Find("Wings").gameObject.GetComponent<SkinnedMeshRenderer>();
+           
+            CharacterModel.RendererInfo[] defaultRender = new CharacterModel.RendererInfo[]
+            {
+                new CharacterModel.RendererInfo
+                {
+                    renderer = bugBodyRenderer,
+                    defaultMaterial = bugBodyRenderer.material,
+                    ignoreOverlays = false,
+                    defaultShadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On,
+                    hideOnDeath = false
+                },
+                new CharacterModel.RendererInfo
+                {
+                    renderer = bugWingsRenderer,
+                    defaultMaterial = bugWingsRenderer.material,
+                    ignoreOverlays = true,
+                    defaultShadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On,
+                    hideOnDeath = false
+                },
+               
+            };
+
+            return new ICharacterModel.CharacterModelParams()
+            {
+                autoPopulateLightInfos = true,
+                renderInfos = defaultRender
+            };
+        }
+
+        protected override SkinDef[] CreateSkinDefs(GameObject modelPrefab)
+        {
+            var bugBodyRenderer = modelPrefab.transform.Find("Bug").gameObject.GetComponent<SkinnedMeshRenderer>();
+            var bugWingsRenderer = modelPrefab.transform.Find("Wings").gameObject.GetComponent<SkinnedMeshRenderer>();
+
+            CharacterModel.RendererInfo[] defaultRender = new CharacterModel.RendererInfo[]
+            {
+                new CharacterModel.RendererInfo
+                {
+                    renderer = bugBodyRenderer,
+                    defaultMaterial = bugBodyRenderer.material,
+                    ignoreOverlays = false,
+                    defaultShadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On,
+                    hideOnDeath = false
+                },
+                new CharacterModel.RendererInfo
+                {
+                    renderer = bugWingsRenderer,
+                    defaultMaterial = bugWingsRenderer.material,
+                    ignoreOverlays = true,
+                    defaultShadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On,
+                    hideOnDeath = false
+                },
+
+            };
+            SkinDefs.Default = Utils.CreateSkinDef("skinArcherBugDefault", modelPrefab, defaultRender);
+
+            return new SkinDef[] {SkinDefs.Default};
+        }
+
+        protected override IEntityStateMachine.EntityStateMachineParams[] EntityStateMachineParams()
+        {
+            return new IEntityStateMachine.EntityStateMachineParams[]
+            {
+                new IEntityStateMachine.EntityStateMachineParams
+                {
+                    name = "Body",
+                    initialState = new EntityStates.SerializableEntityStateType(typeof(ModdedEntityStates.Spitter.SpawnState)),
+                    mainState = new EntityStates.SerializableEntityStateType(typeof(EntityStates.GenericCharacterMain)),
+                },
+                new IEntityStateMachine.EntityStateMachineParams
+                {
+                    name = "Weapon",
+                    initialState = new EntityStates.SerializableEntityStateType(typeof(EntityStates.Idle)),
+                    mainState = new EntityStates.SerializableEntityStateType(typeof(EntityStates.Idle))
+                },
+                new IEntityStateMachine.EntityStateMachineParams()
+                {
+                    name = "Fly",
+                    initialState = new EntityStates.SerializableEntityStateType(typeof(EntityStates.FlyingVermin.Mode.GrantFlight)),
+                    mainState = new EntityStates.SerializableEntityStateType(typeof(EntityStates.FlyingVermin.Mode.GrantFlight))
+                }
+            };
+        }
+
+        protected override bool AddFootstepHandler => false;
+        protected override IFootStepHandler.FootstepHandlerParams FootstepHandlerParams()
+        {
+            return new IFootStepHandler.FootstepHandlerParams();
+        }
+
+        protected override IGenericSkill.GenericSkillParams[] GenericSkillParams()
+        {
+            return new IGenericSkill.GenericSkillParams[]
+             {
+
+             }
+ ;
+        }
+
+        protected override ItemDisplayRuleSet ItemDisplayRuleSet()
+        {
+            var idrs = ScriptableObject.CreateInstance<ItemDisplayRuleSet>();
+            (idrs as ScriptableObject).name = "idrsArcherBug";
+            return idrs;
+        }
+
+        protected override string ModelName() => "Bug";
+
+        protected override IModelPanelParameters.ModelPanelParams ModelPanelParams()
+        {
+            return new IModelPanelParameters.ModelPanelParams()
+            {
+                maxDistance = 6f,
+                minDistance = 1.5f,
+                modelRotation = new Quaternion(0, 0, 0, 1)
+            };
+        }
+
+        protected override SurfaceDef SurfaceDef() => Addressables.LoadAssetAsync<SurfaceDef>("RoR2/Base/Beetle/sdBeetleGuard.asset").WaitForCompletion();
+    }
+}
---
+++ EnemiesReturns/Enemies/ArcherBug/ArcherBugMaster.cs
@@ -0,0 +1,39 @@
+﻿using EnemiesReturns.Components;
+using EnemiesReturns.Components.MasterComponents;
+using RoR2;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace EnemiesReturns.Enemies.ArcherBug
+{
+    public class ArcherBugMaster : MasterBase
+    {
+        public static GameObject MasterPrefab;
+
+        protected override IBaseAI.BaseAIParams BaseAIParams()
+        {
+            var aiParams = base.BaseAIParams();
+            aiParams.graphType = RoR2.Navigation.MapNodeGroup.GraphType.Air;
+            return base.BaseAIParams();
+            
+        }
+
+        protected override IAISkillDriver.AISkillDriverParams[] AISkillDriverParams()
+        {
+            return new IAISkillDriver.AISkillDriverParams[]
+            {
+                new IAISkillDriver.AISkillDriverParams("PathFromAfar")
+                {
+                    skillSlot = SkillSlot.None,
+                    minDistance = 0f,
+                    maxDistance = float.PositiveInfinity,
+                    moveTargetType = RoR2.CharacterAI.AISkillDriver.TargetType.CurrentEnemy,
+                    movementType = RoR2.CharacterAI.AISkillDriver.MovementType.ChaseMoveTarget,
+                    aimType = RoR2.CharacterAI.AISkillDriver.AimType.AtMoveTarget
+                }
+            };
+        }
+    }
+}
