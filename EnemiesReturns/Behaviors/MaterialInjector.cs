826fde43e52d6bc09e74e888b06ecd242ed9b4cc
viliger <wowlocale@gmail.com>
2025-04-12 22:31:23 +0300

    Added cover to arena. Redid ground nodes and rebaked air nodes

---
+++ EnemiesReturns/Behaviors/MaterialInjector.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+namespace EnemiesReturns.Behaviors
+{
+    public class MaterialInjector : MonoBehaviour
+    {
+        public string addressableMaterialPath;
+
+        public bool isParticle;
+
+        public Renderer renderer;
+
+        private void Awake()
+        {
+            if (isParticle)
+            {
+                renderer = GetComponent<ParticleSystemRenderer>();
+            }
+
+            if(renderer && !string.IsNullOrEmpty(addressableMaterialPath))
+            {
+                try
+                {
+                    var material = Addressables.LoadAssetAsync<Material>(addressableMaterialPath).WaitForCompletion();
+                    renderer.material = material;
+                }
+                catch (Exception e) 
+                {
+                    Log.Error($"Error while injecting material into renderer belonging to {this.gameObject.name}: {e}");
+                }
+            }
+        }
+
+    }
+}
