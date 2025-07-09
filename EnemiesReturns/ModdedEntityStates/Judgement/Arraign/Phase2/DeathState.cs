b2299a024ab2d4a3e28f85d6db0b88e0252383cd
viliger <wowlocale@gmail.com>
2025-07-09 19:42:38 +0300

    Added death effect to Arraign P2.

--- EnemiesReturns/ModdedEntityStates/Judgement/Arraign/Phase2/DeathState.cs
+++ EnemiesReturns/ModdedEntityStates/Judgement/Arraign/Phase2/DeathState.cs
@@ -1,15 +1,41 @@
 ﻿using EnemiesReturns.Reflection;
 using EntityStates;
+using RoR2;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
 
 namespace EnemiesReturns.ModdedEntityStates.Judgement.Arraign.Phase2
 {
     [RegisterEntityState]
     public class DeathState : GenericCharacterDeath
     {
+        private AsyncOperationHandle<GameObject> handle;
+
         public override void OnEnter()
         {
             bodyPreservationDuration = 20f;
             base.OnEnter();
+            var handle = Addressables.LoadAssetAsync<GameObject>(RoR2BepInExPack.GameAssetPaths.RoR2_Base_Brother.BrotherDeathEffect_prefab);
+            if (handle.IsValid()) {
+                handle.Completed += (operationResult) =>
+                {
+                    if(operationResult.Status == AsyncOperationStatus.Succeeded)
+                    {
+                        EffectManager.SimpleMuzzleFlash(operationResult.Result, base.gameObject, "MuzzleCenter", transmit: false);
+                    }
+                    Addressables.Release(handle);
+                };
+            }
+        }
+
+        public override void OnExit()
+        {
+            base.OnExit();
+            if(handle.IsValid())
+            {
+                Addressables.Release(handle);
+            }
         }
     }
 }
