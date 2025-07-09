4fc14467729e2b7ed405435d2738ed44a3223b50
viliger <wowlocale@gmail.com>
2025-07-09 23:45:24 +0300

    Fixed Lynx Totem having negative scale box collider on spawn from shaman.

---
+++ EnemiesReturns/EditorHelpers/ColliderScaleDumper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace EnemiesReturns.EditorHelpers
+{
+    public class ColliderScaleDumper : MonoBehaviour
+    {
+        private void FixedUpdate()
+        {
+            Log.Info($"collider scale: {gameObject.transform.localScale}, lossy scale: {gameObject.transform.lossyScale}");
+        }
+    }
+}
