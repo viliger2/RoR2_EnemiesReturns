3f7050ba6d85df33bcd354a734ab01340b1d1293
viliger <wowlocale@gmail.com>
2025-08-03 13:38:37 +0300

    Added soft shadows and PP to Ifrit's Pillar.

---
+++ EnemiesReturns/Behaviors/LightRangeScale.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace EnemiesReturns.Behaviors
+{
+    public class LightRangeScale : MonoBehaviour
+    {
+        public Light light;
+
+        public AnimationCurve lightRangeCurve;
+
+        public float maxDuration;
+
+        public bool destroyOnEnd;
+
+        private float stopwatch;
+
+        private float initialRange;
+
+        private void Awake()
+        {
+            if (!light)
+            {
+                light = GetComponent<Light>();
+            }
+
+            if (!light)
+            {
+                this.enabled = false;
+            }
+
+            initialRange = light.range;
+        }
+
+        private void OnEnable()
+        {
+            stopwatch = 0f;
+        }
+
+        private void Update()
+        {
+            stopwatch += Time.deltaTime;
+            float num = Mathf.Clamp01(stopwatch / maxDuration);
+            light.range = initialRange * lightRangeCurve.Evaluate(num);
+            if(num == 1f && destroyOnEnd)
+            {
+                UnityEngine.Object.Destroy(light.gameObject);
+            }
+        }
+
+
+    }
+}
