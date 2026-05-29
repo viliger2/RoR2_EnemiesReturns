258b3419d9cf0558e17c69cd9928b562ac44bccc
viliger <wowlocale@gmail.com>
2026-05-29 13:01:11 +0300

    Moved Aeonian elite ramp to GradientTexture asset.

---
+++ EnemiesReturns/EditorHelpers/EliteRempPreview.cs
@@ -0,0 +1,36 @@
+﻿using EnemiesReturns.Behaviors;
+using RoR2;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace EnemiesReturns.EditorHelpers
+{
+    public class EliteRempPreview : MonoBehaviour
+    {
+        public Texture2D eliteRamp;
+
+        public Renderer renderer;
+
+        private MaterialPropertyBlock propertyBlock;
+
+        public void Awake()
+        {
+            propertyBlock = new MaterialPropertyBlock();
+        }
+
+        private void FixedUpdate()
+        {
+            SetProperties();
+        }
+
+        private void SetProperties()
+        {
+            renderer.GetPropertyBlock(propertyBlock);
+            propertyBlock.SetTexture(SetEliteRampOnShader.EliteRampPropertyID, eliteRamp);
+            propertyBlock.SetFloat(CommonShaderProperties._EliteIndex, 1);
+            renderer.SetPropertyBlock(propertyBlock);
+        }
+    }
+}
---
+++ EnemiesReturnsThunderkit/Assets/EnemiesReturns/Scripts/EditorHelpers/EliteRempPreview.cs
@@ -0,0 +1,38 @@
+﻿using EnemiesReturns.Behaviors;
+using RoR2;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace EnemiesReturns.EditorHelpers
+{
+    public class EliteRempPreview : MonoBehaviour
+    {
+        public static int EliteRampPropertyID = Shader.PropertyToID("_EliteRamp");
+
+        public Texture2D eliteRamp;
+
+        public Renderer renderer;
+
+        private MaterialPropertyBlock propertyBlock;
+
+        public void Awake()
+        {
+            propertyBlock = new MaterialPropertyBlock();
+        }
+
+        private void FixedUpdate()
+        {
+            SetProperties();
+        }
+
+        private void SetProperties()
+        {
+            renderer.GetPropertyBlock(propertyBlock);
+            propertyBlock.SetTexture(EliteRampPropertyID, eliteRamp);
+            propertyBlock.SetFloat(CommonShaderProperties._EliteIndex, 1);
+            renderer.SetPropertyBlock(propertyBlock);
+        }
+    }
+}
